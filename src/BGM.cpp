e2d31ca04b05907cab34859be95aa4fd42e1dd4a
Annie Huang <63338271+Annie04082020@users.noreply.github.com>
2026-03-13 00:05:01 +0800

    feat: implement background music functionality with a title theme.

--- src/BGM.cpp
+++ src/BGM.cpp
@@ -3,7 +3,4 @@
 BackgroundMusic::BackgroundMusic()
     : m_BGM(RESOURCE_DIR "/Audio/music/title_theme.ogg") {}
 
-void BackgroundMusic::Play_BGM() {
-  m_BGM.Play(-1);
-  //   LOG_INFO("BGM is playing!");
-}
+void BackgroundMusic::Play_BGM() { m_BGM.Play(-1); }
