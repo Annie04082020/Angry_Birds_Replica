b7a942108f6f09d731eca016f9f18ecdb89256cc
anniehuang <annie.huang921002@gmail.com>
2026-03-24 23:12:56 +0800

    style: Refactor IntroScene constructor and methods for improved readability

--- src/IntroScene.cpp
+++ src/IntroScene.cpp
@@ -2,13 +2,15 @@
 #include "BGM.hpp"
 #include "Resource.hpp"
 
-std::shared_ptr<IntroScene> IntroScene::Create() {
+std::shared_ptr<IntroScene> IntroScene::Create()
+{
     auto bg = std::make_shared<DynamicBackground>(Resource::MOVING_BG_IMAGE);
     return std::shared_ptr<IntroScene>(new IntroScene(bg));
 }
 
 IntroScene::IntroScene(std::shared_ptr<DynamicBackground> bg)
-    : Scene(bg), m_movingBg(bg) {
+    : Scene(bg), m_movingBg(bg)
+{
     SetVisible(false);
     SetZIndex(50);
     SetBGM(std::make_shared<BackgroundMusic>(Resource::TITLE_THEME));
@@ -19,26 +21,27 @@ IntroScene::IntroScene(std::shared_ptr<DynamicBackground> bg)
     m_playbutton = std::make_shared<Button>(Resource::Play_Button);
     m_playbutton->SetZIndex(50);
     m_playbutton->SetVisible(true);
-    m_playbutton->SetOnClickFunction([this]() {
+    m_playbutton->SetOnClickFunction([this]()
+                                     {
         m_playbutton->SetVisible(false);
-        m_bird->SetVisible(true);
-    });
+        m_bird->SetVisible(true); });
 
     m_exitbutton = std::make_shared<Button>(Resource::Exit_Button);
     m_exitbutton->SetZIndex(50);
     m_exitbutton->SetPosition({-1000, -550});
     m_exitbutton->SetVisible(true);
-    m_exitbutton->SetOnClickFunction([this]() {
+    m_exitbutton->SetOnClickFunction([this]()
+                                     {
         m_exitbutton->SetVisible(false);
-        m_bird->SetVisible(true);
-    });
+        m_bird->SetVisible(true); });
 
     m_settingbutton = std::make_shared<Button>(Resource::Setting_Button);
     m_settingbutton->SetZIndex(50);
     m_settingbutton->SetPosition({1000, -550});
     m_settingbutton->SetVisible(true);
     m_settingbutton->SetSFX(Resource::SETTING_SFX);
-    m_settingbutton->SetOnClickFunction([this]() { m_bird->SetVisible(true); });
+    m_settingbutton->SetOnClickFunction([this]()
+                                        { m_bird->SetVisible(true); });
 
     AddElements(m_bird);
     AddElements(m_playbutton);
@@ -46,7 +49,8 @@ IntroScene::IntroScene(std::shared_ptr<DynamicBackground> bg)
     AddElements(m_settingbutton);
 }
 
-void IntroScene::Update() {
+void IntroScene::Update()
+{
     m_movingBg->Update();
     Scene::Update();
 }
