973be82662dc9feb59c5ed5a1c5dab07eb1e6c40
anniehuang <annie.huang921002@gmail.com>
2026-03-29 22:40:42 +0800

    Internalize PTSD framework and add global camera zoom support

---
+++ PTSD/include/config.hpp
@@ -0,0 +1,25 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "pch.hpp" // IWYU pragma: export
+
+#include "Util/Logger.hpp"
+
+constexpr const char *TITLE = "Angry Birds";
+
+constexpr int WINDOW_POS_X = SDL_WINDOWPOS_UNDEFINED;
+constexpr int WINDOW_POS_Y = SDL_WINDOWPOS_UNDEFINED;
+
+constexpr unsigned int WINDOW_WIDTH = 2400;
+constexpr unsigned int WINDOW_HEIGHT = 1350;
+
+constexpr Util::Logger::Level DEFAULT_LOG_LEVEL = Util::Logger::Level::DEBUG;
+
+/**
+ * @brief FPS limit
+ *
+ * Set value to 0 to turn off FPS cap
+ */
+constexpr unsigned int FPS_CAP = 60;
+
+#endif
